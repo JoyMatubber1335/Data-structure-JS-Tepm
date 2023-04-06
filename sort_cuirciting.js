addd53dce1531f009be6b932d7c387b9a55f9413
JoyMatubber <joy.matubber@Brainstation-23.com>
2023-04-07 03:04:07 +0600

    Complete Logical Assignment Operator with short cuircting

--- sort_cuirciting.js
+++ sort_cuirciting.js
@@ -15,3 +15,34 @@ let a = 0;
 console.log(v ?? 23); // undefine handle kore na so print undefine
 console.log(a ?? 23); // 0  ke convert kore  0
 console.log('' ?? 23); // '' o kaj kore
+
+// Assign ment Operator almost same as sort_cuircting
+
+//--------OR-----------
+
+const obj1 = {
+  name: 'joy',
+  age: 23,
+};
+const obj2 = {
+  name: 'rana',
+  dep: 'cse',
+  age: 0,
+};
+
+obj1.age = obj1.age || 10; // use sort but we use operator
+console.log(obj1.age);
+obj1.age ||= 10; // use Or operator
+console.log(obj1.age);
+// const o1= obj1.age||=10;
+
+obj2.age = obj2.age && 10;
+console.log(obj2.age);
+obj2.age &&= 23;
+console.log(obj2.age);
+// for nulish handle
+console.log('Nulish');
+obj2.age = obj2.age ?? 23;
+console.log(obj2.age);
+obj2.age ??= 23;
+console.log(obj2.age);
